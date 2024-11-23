987ec37462861d9e536ffeb51c6bf36f617b9e4e
yedinorozhik <misterrpandaa@gmail.com>
2024-11-23 15:26:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1000;
+        int mile = price / 20;
+
+        System.out.println("Начислено миль:" + mile);
+    }
+}

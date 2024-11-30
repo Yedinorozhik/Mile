4f90b0b8fb7a4e64069788e5ba77f45b24685407
yedinorozhik <misterrpandaa@gmail.com>
2024-11-30 13:17:15 +0300

    Calculate

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 1000;
-        int mile = price / 20;
-
-        System.out.println("Начислено миль:" + mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 25_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
